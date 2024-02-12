f9eedb3bdeb80a138c5bc3084a64d88cbf8104f4
andreihar <95883512+andreihar@users.noreply.github.com>
2024-02-11 19:39:52 -0800

    Updated Footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -5,7 +5,8 @@ function Footer() {
 		<footer className="py-3 my-4">
 			<ul className="nav justify-content-center border-bottom pb-3 mb-3">
 				<li className="nav-item"><Link to='/' className="nav-link px-2 text-body-secondary">Home</Link></li>
-				<li className="nav-item"><Link to='/form' className="nav-link px-2 text-body-secondary">New Recipe</Link></li>
+				<li className="nav-item"><Link to='/form' className="nav-link px-2 text-body-secondary">Add Recipe</Link></li>
+				<li className="nav-item"><Link to='/contributors' className="nav-link px-2 text-body-secondary">Contributors</Link></li>
 			</ul>
 			<p className="text-center text-body-secondary">© 2024 Andrei Harbachov</p>
 		</footer>
