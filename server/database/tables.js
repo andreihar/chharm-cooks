bd8d55a439283ae46f353e2d210c0473afaf6d4b
andreihar <95883512+andreihar@users.noreply.github.com>
2024-04-24 19:27:16 -0700

    Redefined database to excluse User tables

--- server/database/tables.js
+++ server/database/tables.js
@@ -1,18 +1,3 @@
-const createUsersTable = `
-	CREATE TABLE IF NOT EXISTS users(
-		username VARCHAR(50) PRIMARY KEY,
-		password VARCHAR(60) NOT NULL,
-		email VARCHAR(255) UNIQUE NOT NULL,
-		picture TEXT,
-		social TEXT,
-		first_name VARCHAR(50),
-		last_name VARCHAR(50),
-		bio TEXT,
-		occupation VARCHAR(100),
-		created_on TIMESTAMPTZ DEFAULT NOW()
-	);
-`;
-
 const createIngredientTable = `
 	CREATE TABLE IF NOT EXISTS ingredient(
 		iid SERIAL PRIMARY KEY, 
@@ -26,8 +11,7 @@ const createRecipeTable = `
 		title VARCHAR(50),
 		chin_title VARCHAR(50),
 		cuisine VARCHAR(50),
-		username VARCHAR(50),
-		FOREIGN KEY (username) REFERENCES users(username),
+		username VARCHAR(255),
 		prep_time INT,
 		cook_time INT,
 		servings INT,
@@ -42,37 +26,33 @@ const createRecipeTable = `
 
 const createFollowersTable = `
 	CREATE TABLE IF NOT EXISTS followers(
-		follower VARCHAR(50),
-		followed VARCHAR(50),
-		PRIMARY KEY (follower, followed),
-		FOREIGN KEY (follower) REFERENCES users(username) ON DELETE CASCADE,
-		FOREIGN KEY (followed) REFERENCES users(username) ON DELETE CASCADE
+		follower VARCHAR(255),
+		followed VARCHAR(255),
+		PRIMARY KEY (follower, followed)
 	);
 `;
 
 const createLikesTable = `
 	CREATE TABLE IF NOT EXISTS likes(
-		username VARCHAR(50),
+		username VARCHAR(255),
 		rid SERIAL,
 		PRIMARY KEY (username, rid),
-		FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
 		FOREIGN KEY (rid) REFERENCES recipe(rid) ON DELETE CASCADE
 	);
 `;
 
 const createNotificationsTable = `
 	CREATE TABLE IF NOT EXISTS notifications(
-		username VARCHAR(50),
+		username VARCHAR(255),
 		rid INT,
 		read BOOLEAN DEFAULT FALSE,
 		PRIMARY KEY (username, rid),
-		FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
 		FOREIGN KEY (rid) REFERENCES recipe(rid) ON DELETE CASCADE
 	);
 `;
 
 module.exports = {
-	createUsersTable,
+	// createUsersTable,
 	createIngredientTable,
 	createRecipeTable,
 	createFollowersTable,
